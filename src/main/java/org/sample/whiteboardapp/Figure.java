fd010d1174b65b48769542dd1a1c6bb291ac20dd
Brian S. Sheldon <brian.s.sheldon@gmail.com>
2017-04-07 08:08:59 -0500

    initial

---
+++ src/main/java/org/sample/whiteboardapp/Figure.java
@@ -0,0 +1,29 @@
+package org.sample.whiteboardapp;
+
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class Figure {
+
+    public Figure(JsonObject json) {
+        this.json = json;
+    }
+
+    private JsonObject json;
+
+    public JsonObject getJson() {
+        return json;
+    }
+
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
+
+    @Override
+    public String toString() {
+        StringWriter writer = new StringWriter();
+        Json.createWriter(writer).write(json);
+        return writer.toString();
+    }
+}
